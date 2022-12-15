082bc0907d826550ec918c2693918a40f1358cf1
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2022-12-15 18:46:54 +0530

    Time: 3 ms (73.18%), Space: 53.1 MB (79.60%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int n = nums.length;
+        int less_than_count = 1;
+        int greater_than_count = 1;
+        for(int i =0; i<n-1; i++){
+            if(nums[i] <= nums[i+1]){
+                less_than_count++;
+            }
+            if(nums[i] >= nums[i+1]){
+                greater_than_count++;
+            }
+        }
+        if(less_than_count == n){
+            return true;
+        }
+        if(greater_than_count == n){
+            return true;
+        }
+        
+        return false;
+    }
+}
