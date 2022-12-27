6669620dcf4126bc429c1761514cae00d0483064
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2022-12-27 23:59:26 +0530

    Time: 5 ms (99.99%), Space: 43.9 MB (79.38%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,28 +9,39 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> l1 = new ArrayList<>();
-        ArrayList<TreeNode> l2 = new ArrayList<>();
-        isPresent(root,p.val, l1);
-        isPresent(root,q.val, l2);
+        // optimize approach :
+        if(root == null) return null;
+        if(root.val == p.val || root.val == q.val) return root;
+        
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        
+        if(right == null) return left;
+        if(left == null) return right;
+        
+        return root;
+//         ArrayList<TreeNode> l1 = new ArrayList<>();
+//         ArrayList<TreeNode> l2 = new ArrayList<>();
+//         isPresent(root,p.val, l1);
+//         isPresent(root,q.val, l2);
        
-        int i = l1.size()-1;
-        int j = l2.size()-1;
-        while(i>=0 && j>=0 && l1.get(i).val == l2.get(j).val){
-            i--;
-            j--;
-        }
-        if(i == l1.size()-1) return null;
-        return l1.get(i+1);
-    }
+//         int i = l1.size()-1;
+//         int j = l2.size()-1;
+//         while(i>=0 && j>=0 && l1.get(i).val == l2.get(j).val){
+//             i--;
+//             j--;
+//         }
+//         if(i == l1.size()-1) return null;
+//         return l1.get(i+1);
+//     }
     
-    public static boolean isPresent(TreeNode root,int target, ArrayList<TreeNode> arr){
-        if(root == null) return false;
-        if(root.val == target || isPresent(root.left,target, arr) || isPresent(root.right,target,arr)){
-            arr.add(root);
-            return true;
-        }
-        return false;
+//     public static boolean isPresent(TreeNode root,int target, ArrayList<TreeNode> arr){
+//         if(root == null) return false;
+//         if(root.val == target || isPresent(root.left,target, arr) || isPresent(root.right,target,arr)){
+//             arr.add(root);
+//             return true;
+//         }
+//         return false;
     }
     
 }
