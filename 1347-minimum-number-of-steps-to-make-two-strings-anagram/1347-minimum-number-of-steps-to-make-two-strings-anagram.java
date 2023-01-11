498ba38bce8d50a7088571b1f930ece754fa1832
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-01-12 01:40:30 +0530

    Time: 8 ms (95.80%), Space: 43 MB (80.02%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,28 +1,15 @@
 class Solution {
+
     public int minSteps(String s, String t) {
+        int count[] = new int[26];
+
+        for (char ch : s.toCharArray()) count[ch - 'a']++;
+        for (char ch : t.toCharArray()) count[ch - 'a']--;
+
         int ans = 0;
-        HashMap<Character, Integer> sFreq = new HashMap();
-        HashMap<Character, Integer> tFreq = new HashMap();
-        
-        for(char ch : s.toCharArray()){
-            sFreq.put(ch, sFreq.getOrDefault(ch, 0) + 1);
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0) ans += count[i];
         }
-        
-        for(char ch : t.toCharArray()){
-
-            tFreq.put(ch, tFreq.getOrDefault(ch, 0) + 1);
-        }
-
-        for (Map.Entry<Character, Integer> entry : sFreq.entrySet()){
-            if(tFreq.containsKey(entry.getKey())){
-                int diff = entry.getValue() - tFreq.get(entry.getKey());
-                if(diff > 0)
-                    ans += diff;
-            } else {
-                ans += entry.getValue();
-            }
-        }
-        
         return ans;
     }
 }
