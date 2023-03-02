de4df69163066928e76b4c816529b11fb592cc9f
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-03-02 15:00:58 +0530

    Time: 10 ms (11.59%), Space: 43.4 MB (18.60%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int compress(char[] chars) {
+        String str = compression(chars);
+        for(int i = 0 ; i<str.length();i++){
+            chars[i] = str.charAt(i);
+        }
+        return str.length();
+    }
+
+    public String compression(char[] chars){
+        String str = ""+chars[0];
+        int counter = 1;
+        for(int i = 1 ; i < chars.length ; i++){
+            char curr = chars[i];
+            char prev = chars[i-1];
+            if(curr==prev){
+                counter++;
+            }else{
+                if(counter>1){
+                    str+=counter;
+                    counter=1;
+                }
+                str+=curr;
+            }
+        } 
+        if(counter!=1){
+            str+=counter;
+        }
+        return str;
+    }
+}
