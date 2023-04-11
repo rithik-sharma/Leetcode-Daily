4f306d038e10868c66654b0fdcc90a5bc470d559
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-04-11 14:18:10 +0530

    Time: 3 ms (37.67%), Space: 43.8 MB (12.86%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,25 +1,40 @@
 class Solution {
     public int findCircleNum(int[][] M) {
-        int n = M.length;
+         int n = M.length;
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<ArrayList<Integer>>();
+        for(int i =0; i<n; i++){
+            adjList.add(new ArrayList<Integer>());
+        }
+        
+        //adjacency List
+        for(int i =0; i<n; i++){
+            for(int j =0; j<M[0].length; j++){
+                if(M[i][j] == 1 && i != j){
+                    adjList.get(i).add(j);
+                    adjList.get(j).add(i);
+                }
+            }
+        }
+       
         int vis[] = new int[n];
         int count = 0;
         
         for(int i = 0; i < n ;i++){
             if(vis[i] == 0){
                 count++;
-                dfs(M,i,vis);
+                dfs(adjList,i,vis);
             }
         }
       return count;  
     }
     
     
-    private void dfs(int[][]M,int i, int vis[]){
-        for(int j = 0 ; j < M[i].length ; j++){
-            if(vis[j] == 0 && M[i][j] != 0){
-                vis[j] = 1;
-                dfs(M,j,vis);
-            }
+    private void dfs(ArrayList<ArrayList<Integer>> adjList,int i, int vis[]){
+       vis[i] = 1;
+            for(Integer it : adjList.get(i)){
+                if(vis[it] == 0){
+                    dfs(adjList, it , vis);
+                }
         }
     }
     
