2d7c6b951b78f49eb80770d797be34419da78eae
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-05-30 11:55:47 +0530

    Time: 1 ms (72.17%), Space: 44.8 MB (14.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,45 +1,92 @@
-class Solution {
+// class Solution {
+// public void setZeroes(int[][] matrix) {
+      // Brute force solution
+        
+//         //col
+//         int m = matrix[0].length;
+//         //row
+//         int n = matrix.length;
+//         for(int i =0; i<n; i++){
+//             for(int j =0; j<m; j++){
+//                 if(matrix[i][j] == 0){
+//                   markRow(matrix,i,m);
+//                   markCol(matrix,j,n);  
+//                 } 
+//             }
+//         }
+        
+//          for(int i =0; i<n; i++){
+//             for(int j =0; j<m; j++){
+//                 if(matrix[i][j] == -4){
+//                     matrix[i][j] = 0;
+//                 } 
+//             }
+//         }
+       
+//     }
+    
+//     public void markRow(int[][] matrix,int i , int m){
+//         for(int j =0; j<m; j++){
+//             if(matrix[i][j] != 0){
+//                 matrix[i][j] = -4;
+//             }
+//         }
+//     }
+    
+//       public void markCol(int[][] matrix,int j , int n){
+//         for(int i =0; i<n; i++){
+//             if(matrix[i][j] != 0){
+//                 matrix[i][j] = -4;
+//             }
+//         }
+//     }
+
+        
+//         Here, i just take -4 this value should be just somthing different.
+//         Here's the time complexity is 
+//         first nested loops n*m inside it n+m, so we can write it like (n*m) * (n+m)
+//         And then at last we itrate over the matrix again n*m.
+//         so the tc becomes (n*m) * (n+m) + n*m  so it become someWhat  ~  O(n^3)
+        
+        
+        
+//         BETTER SOOLUTION :-
+
+ class Solution {
     public void setZeroes(int[][] matrix) {
-        // Brute force solution
-        //col
-        int m = matrix[0].length;
-        //row
+//     row
         int n = matrix.length;
+//     col
+        int m = matrix[0].length;
+        
+        int[] arrRow = new int[n];
+        int[] arrCol = new int[m];
+        
         for(int i =0; i<n; i++){
             for(int j =0; j<m; j++){
                 if(matrix[i][j] == 0){
-                  markRow(matrix,i,m);
-                  markCol(matrix,j,n);  
-                } 
+                    arrRow[i] = 1;
+                    arrCol[j] = 1;
+                }
             }
         }
         
-         for(int i =0; i<n; i++){
-            for(int j =0; j<m; j++){
-                if(matrix[i][j] == -4){
-                    matrix[i][j] = 0;
-                } 
-            }
-        }
-       
-    }
-    
-    public void markRow(int[][] matrix,int i , int m){
-        for(int j =0; j<m; j++){
-            if(matrix[i][j] != 0){
-                matrix[i][j] = -4;
-            }
-        }
-    }
-    
-      public void markCol(int[][] matrix,int j , int n){
         for(int i =0; i<n; i++){
-            if(matrix[i][j] != 0){
-                matrix[i][j] = -4;
+            for(int j =0; j<m; j++){
+                if(arrRow[i] == 1 || arrCol[j] == 1){
+                    matrix[i][j] = 0;
+                }
             }
         }
-    }
     
     
     
-}
+     }
+ }
+        
+        
+        
+        
+    
+    
+    
