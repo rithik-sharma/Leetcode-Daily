7b86959cc11376c054633d8af07d90fdec81584a
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-05-30 13:11:07 +0530

    Time: 1 ms (72.17%), Space: 44.6 MB (22.80%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,6 +1,6 @@
 // class Solution {
 // public void setZeroes(int[][] matrix) {
-      // Brute force solution
+// --------------------------     Brute force solution    ----------------------------
         
 //         //col
 //         int m = matrix[0].length;
@@ -50,36 +50,87 @@
         
         
         
-//         BETTER SOOLUTION :-
+//  ------------------------       BETTER SOOLUTION :-      ------------------------------
+
+//  class Solution {
+//     public void setZeroes(int[][] matrix) {
+// //     row
+//         int n = matrix.length;
+// //     col
+//         int m = matrix[0].length;
+        
+//         int[] arrRow = new int[n];
+//         int[] arrCol = new int[m];
+        
+//         for(int i =0; i<n; i++){
+//             for(int j =0; j<m; j++){
+//                 if(matrix[i][j] == 0){
+//                     arrRow[i] = 1;
+//                     arrCol[j] = 1;
+//                 }
+//             }
+//         }
+        
+//         for(int i =0; i<n; i++){
+//             for(int j =0; j<m; j++){
+//                 if(arrRow[i] == 1 || arrCol[j] == 1){
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+    
+    
+//      }
+//  }
+// Discuss Time Complexity :-
+// we are just itrating 2 time n*m and thus the tc would be O(n^2) time approx...
+// but here we take some space O(m+n)
+
+
+//-------------------------       Optimal Solution :-       ----------------------------
 
  class Solution {
     public void setZeroes(int[][] matrix) {
-//     row
+        //     row
         int n = matrix.length;
-//     col
+        //     col
         int m = matrix[0].length;
         
-        int[] arrRow = new int[n];
-        int[] arrCol = new int[m];
+        int col0 = 1;
+     for(int i =0; i<n; i++){
+        for(int j =0; j<m;j++){
+            if(matrix[i][j] == 0){
+                // mark the ith row
+                matrix[i][0] = 0;
+                // mark the jth col
+                if(j != 0){
+                    matrix[0][j] = 0;
+                }else{
+                    col0 = 0;
+                }
+            }
+        }
+      }
         
-        for(int i =0; i<n; i++){
-            for(int j =0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    arrRow[i] = 1;
-                    arrCol[j] = 1;
+        for(int i =1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(matrix[i][j] != 0){
+                    // check for col & row
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                        matrix[i][j] = 0;
+                    }
+                    
                 }
             }
         }
         
-        for(int i =0; i<n; i++){
-            for(int j =0; j<m; j++){
-                if(arrRow[i] == 1 || arrCol[j] == 1){
-                    matrix[i][j] = 0;
-                }
-            }
+        if(matrix[0][0] == 0){
+            for(int j =0; j<m; j++) matrix[0][j] = 0;
         }
-    
-    
+        
+        if(col0 == 0){
+            for(int i =0; i<n; i++) matrix[i][0] = 0; 
+        }
     
      }
  }
