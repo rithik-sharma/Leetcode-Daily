a496cc017b7710cbab961f3020ab183645233c37
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-06-01 23:51:44 +0530

    Time: 1 ms (74.10%), Space: 41.8 MB (98.33%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,16 +1,26 @@
 class Solution {
-   public List<List<Integer>> subsets(int[] nums){  
-    List<List<Integer>> res = new ArrayList();
-       res.add(new ArrayList());
+   public List<List<Integer>> subsets(int[] arr){  
+       int n = arr.length;
+    List<List<Integer>> res = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+       solve(arr,res,0,n,cur);
        
-       for(int num : nums){
-           int n = res.size();
-           for(int j =0; j<n; j++){
-               List<Integer> temp = new ArrayList(res.get(j));
-               temp.add(num);
-               res.add(temp);
-           }
-       }
        return res;
    }
-}
+
+    public static void solve(int[] arr,List<List<Integer>> res, int i, int n,List<Integer> cur){
+       
+        if(i == n){
+            res.add(new ArrayList<Integer>(cur));
+            return;
+        }
+        cur.add(arr[i]);
+        solve(arr, res, i+1, n, cur);
+        cur.remove(cur.size() - 1);
+        solve(arr, res, i+1, n, cur);
+    }
+              
+ }
+
+// TC = O(n*2^n)
+// Sc = o(n*2^n)
