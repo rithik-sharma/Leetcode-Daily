5c0e18d0d440ecef4733f4e0a45b6b82c8994bea
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-06-25 12:08:59 +0530

    Time: 63 ms (78.40%), Space: 44.4 MB (77.37%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -3,23 +3,21 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
-    let currentCount = init;
-    function increment(){
-        currentCount = currentCount + 1;
-        return currentCount;
-    }
-     function decrement(){
-        currentCount = currentCount - 1;
-        return currentCount;
-    }
-    
-    function reset(){
-        currentCount = init;
-        return currentCount;
-    }
-    
-    return {increment, decrement, reset};
-    
+    let c=init;
+    return{
+        increment:function(){
+            c++;
+          return c;
+        },
+       decrement:function(){
+           c--;
+           return c;
+       },
+       reset:function(){
+           c=init;
+           return c;
+       }
+    };
 };
 
 /**
