fe353637fa41df0adf342376779206b5e952e446
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-06-27 01:03:37 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (92.62%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -26,20 +26,40 @@
 
 
 // ---------------> Tabulation <--------------------
+// class Solution {
+//     public int fib(int n) {
+//        int[] dp = new int[n+1];
+//         Arrays.fill(dp, -1);
+        
+//         if(n<=1) return n;
+//         dp[0] = 0;
+//         dp[1] = 1;
+        
+//         for(int i =2; i<=n; i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+        
+//         return dp[n];
+//     }
+    
+// }
+
+
+// ---------------> Space Optimization <--------------------
+
 class Solution {
     public int fib(int n) {
-       int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        
         if(n<=1) return n;
-        dp[0] = 0;
-        dp[1] = 1;
+        int prev2 = 0;
+        int prev = 1;
         
         for(int i =2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
         
-        return dp[n];
+        return prev;
     }
     
 }
