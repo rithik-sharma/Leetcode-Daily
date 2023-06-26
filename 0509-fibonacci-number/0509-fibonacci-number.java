0be57dee8be917a30bf3b3c77252c495d51c9ace
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-06-27 00:38:24 +0530

    Time: 0 ms (100.00%), Space: 39 MB (92.80%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,23 +1,53 @@
+// ---------------> Memoization <--------------------
+
+// class Solution {
+//     public int fib(int n) {
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp, -1);
+        
+//         int ans = dp1(n, dp);
+        
+//         return ans;
+        
+//     }
+    
+//     public int dp1(int n , int[] dp){
+//         if(n <= 1){
+//             return n;
+//         }
+        
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }
+        
+//         return dp[n] = dp1(n-1, dp) + dp1(n-2, dp);
+//     }
+// }
+
+
+// ---------------> Tabulation <--------------------
 class Solution {
     public int fib(int n) {
-        int[] dp = new int[n+1];
+       int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
         
-        int ans = dp1(n, dp);
+        int ans = tabulation(n, dp);
         
         return ans;
+    }
+    
+    
+    public int tabulation(int n , int[] dp){
+        if(n<=1) return n;
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i =2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
         
     }
     
-    public int dp1(int n , int[] dp){
-        if(n <= 1){
-            return n;
-        }
-        
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        
-        return dp[n] = dp1(n-1, dp) + dp1(n-2, dp);
-    }
 }
