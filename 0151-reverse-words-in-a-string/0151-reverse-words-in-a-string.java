a8362918d2c8b0e4ba2e81d0bb4cdfaa7aa2e64b
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-08-04 11:17:24 +0530

    Time: 4 ms (95.34%), Space: 41.3 MB (98.76%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,26 +1,54 @@
 class Solution {
     public String reverseWords(String s) {
-        s.trim();
-        String[] words = s.split(" ");
+    String g = removeExtraSpace(s);
+       char[] c = g.toCharArray();
+        int n = c.length;
         
-        int left = 0;
-        int right = words.length - 1;
-        while( left < right){
-            String temp = words[left];
-            words[left] = words[right];
-            words[right] = temp;
-            left++;
-            right--;
-             
+        reverseChar(c , 0 , n-1);
+        reverseWords(c);
+       
+       return new String(c).trim(); 
+    }
+    
+    static void reverseChar(char[] c , int i , int j){
+        while(i < j){
+            char temp = c[i];
+            c[i] = c[j];
+            c[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    
+    static void reverseWords(char[] c){
+        int start = 0;
+        int end = 0;
+        for(; end < c.length; end++){
+            if(c[end] == ' '){
+                reverseChar(c , start , end -1);
+                start = end + 1;
+            }
+        }
+        reverseChar(c, start , end-1);
+    }
+    
+    
+    static String removeExtraSpace(String s){
+        StringBuilder result = new StringBuilder();
+        boolean spaceEncounter = false;
+        
+        for(char c : s.toCharArray()){
+            if(c == ' '){
+                if(!spaceEncounter){
+                    result.append(c);
+                    spaceEncounter = true;
+                }
+            }else{
+                result.append(c);
+                spaceEncounter = false;
+            }
         }
         
-        StringBuilder ans = new StringBuilder();
-        for(String word : words){
-            if(!word.isEmpty())
-            ans.append(word).append(" ");
-        }
-        
-        
-        return ans.toString().trim();
+        return result.toString();
     }
 }
