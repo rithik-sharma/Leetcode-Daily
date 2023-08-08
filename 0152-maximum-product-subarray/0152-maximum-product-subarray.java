a93393f5cfa3f212017f88db6661de6e037eee12
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-08-08 13:41:06 +0530

    Time: 1 ms (85.58%), Space: 43.3 MB (77.24%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,15 +1,26 @@
 class Solution {
-    public int maxProduct(int[] nums) {
-        int maxProduct = Integer.MIN_VALUE;
-        int n = nums.length;
-     for(int i =0; i<n; i++){
-         int product = 1;
-         for(int j =i; j<n; j++){
-             product = product*nums[j];
-             maxProduct = Math.max(maxProduct,product);
-         }
-     }
+    public int maxProduct(int[] n) {
+        int prefix =1;
+        int suffix =1;
+        int max=Integer.MIN_VALUE;
+
+        for(int i=0;i<n.length;i++){
+           
+            if(prefix ==0)prefix=1;
+            if(suffix==0)suffix=1;
+             prefix *= n[i];
+            suffix *= n[n.length-1-i];
+
+            max=Math.max(max,Math.max(prefix,suffix));
+        }
+        return max;
         
-        return maxProduct;
     }
 }
+
+// Intuition
+
+// Imagine if all the no are positive, than we are going to multiply all the no for maximum subarray
+// Imagine if there are even negative no, then also we are going to multiply the all the no to get the maximum subarray
+// Now imagine if there are odd negative no now we cannot multiply all the no now we have to decide which no we have to ignore for getting the maximum subarray.
+// Now if I observe whenever i decide to ignore a negative no then there is always going to be suffix and prefix as a answer
