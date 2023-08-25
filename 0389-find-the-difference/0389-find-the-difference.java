ff7d61d188dcd9fc2743856a7414f862f08f115b
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-08-25 11:36:40 +0530

    Time: 1 ms (100.00%), Space: 41.1 MB (26.66%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,22 +1,45 @@
+// class Solution {
+//     public char findTheDifference(String s, String t) {
+//         int[] dict = new int[128];
+//         // iterate over the first string and increment each appearing character
+//         for (char ch : s.toCharArray()) {
+//             dict[ch]++;
+//         }
+        
+//         // iterate over the second string and decrement each appearing character
+//         for (char ch : t.toCharArray()) {
+//             dict[ch]--;
+//         }
+        
+//          // return the first negative value
+//         for (int i = 0; i < dict.length; i++) {
+//             if (dict[i] < 0) {
+//                 return (char)(i);
+//             }
+//         }
+//         return ' ';
+//     }
+// }
+
+
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] dict = new int[128];
-        // iterate over the first string and increment each appearing character
-        for (char ch : s.toCharArray()) {
-            dict[ch]++;
+       int[] dict = new int[26];
+        
+        for(char c : s.toCharArray()){
+            dict[c - 'a']++;
         }
         
-        // iterate over the second string and decrement each appearing character
-        for (char ch : t.toCharArray()) {
-            dict[ch]--;
+        for(char c : t.toCharArray()){
+            dict[c - 'a']--;
         }
         
-         // return the first negative value
-        for (int i = 0; i < dict.length; i++) {
-            if (dict[i] < 0) {
-                return (char)(i);
+        for(int i =0; i<26; i++){
+            if(dict[i] < 0){
+                return (char)(i + 'a');
             }
         }
+        
         return ' ';
     }
 }
