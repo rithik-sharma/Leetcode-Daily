3af697530788ff08a74312ae75db26f8b3c4387b
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-08-29 10:23:19 +0530

    Time: 1 ms (100.00%), Space: 53.5 MB (31.06%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,26 +1,31 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
+       int n = nums.length;
+       int[] prefix = new int[n];
+       int[] suffix = new int[n];
+        
+        prefix[0] = 1;
+        suffix[n-1] = 1;
+        
+        for(int i =1; i<n; i++){
+           prefix[i] = prefix[i-1] * nums[i-1];
+        }
+         
+//         for(int i = 0; i<n; i++)
+//         System.out.print(prefix[i]);
+        
+        for(int i = n-2; i>=0; i--){
+           suffix[i] = suffix[i+1] * nums[i+1];
+        }
+        
+       // for(int i = 0; i<n; i++)
+       //  System.out.print(suffix[i]);
+        
         int[] ans = new int[n];
-        
-        
-        int leftProd = 1;
-        for(int i =0; i<nums.length; i++){
-            left[i] = leftProd;
-            leftProd = leftProd * nums[i];
+        for(int i =0; i<n; i++){
+            ans[i] = prefix[i]*suffix[i];
         }
         
-        int rightProd = 1;
-        for(int i=nums.length-1;i>=0;i--){
-            right[i] = rightProd;
-            rightProd = rightProd * nums[i];
-        }
-        
-        for(int q=0;q<n;q++){
-           ans[q]=left[q]*right[q];
-       }
-       return ans;
+        return ans;
     }
 }
