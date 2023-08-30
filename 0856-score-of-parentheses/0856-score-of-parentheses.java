178e1fecfaced70f307b1432893cdd9b693cc8f4
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-08-30 15:30:10 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (48.37%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.java
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -1,16 +1,38 @@
+// class Solution {
+//     public int scoreOfParentheses(String s) {
+//        ArrayDeque<Integer> st = new ArrayDeque<>();
+//         int score = 0;
+//         for(int i =0; i<s.length(); i++){
+//             if(s.charAt(i) == '('){
+//                 st.push(score);
+//                 score = 0;
+//             }else{
+//                 score = st.peek() + Math.max(2*score , 1);
+//                 st.pop();
+//             }
+//         }
+//         return score;
+//     }
+// }
+
+
 class Solution {
     public int scoreOfParentheses(String s) {
-       ArrayDeque<Integer> st = new ArrayDeque<>();
-        int score = 0;
-        for(int i =0; i<s.length(); i++){
-            if(s.charAt(i) == '('){
-                st.push(score);
-                score = 0;
-            }else{
-                score = st.peek() + Math.max(2*score , 1);
-                st.pop();
+       int depth = 0;
+       int res =0;
+        char prev = '(';
+        
+        for(char ch : s.toCharArray()){
+            if(ch == '(')
+                depth++;
+            else{
+                depth--;
+                if(prev == '('){
+                    res = res + (int)Math.pow(2,depth);
+                }
             }
+             prev = ch;
         }
-        return score;
+        return res;
     }
 }
