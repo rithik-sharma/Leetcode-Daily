13a1ba1058d4981987deec235e11d1642d1c2725
Rithik Sharma <78314622+rithik-sharma@users.noreply.github.com>
2023-09-11 19:29:43 +0530

    Time: 22 ms (69.84%), Space: 45.7 MB (67.76%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,7 +1,8 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        Map<Integer, Integer> left = new HashMap(),
-            right = new HashMap(), count = new HashMap();
+        Map<Integer, Integer> left = new HashMap();
+        Map<Integer, Integer> right = new HashMap();
+        Map<Integer, Integer> count = new HashMap();
 
         for (int i = 0; i < nums.length; i++) {
             int x = nums[i];
@@ -20,3 +21,40 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+// Dry Run for this test case:-
+// [1,2,2,3,1,3,3,4,2]
+
+// x = 1;
+// left = 1,0
+// right = 1,0
+// count = 1,1
+    
+// x = 2;
+// left = 1,0   2,1  3,3  4,7 
+// right = 1,4  2,8  3,6
+// count = 1,2   2,3   3,3  4,1   
+    
+    
+    
+// ans = 9;
+// degree = 3;
+
+// for (int x: count.keySet()) {
+//     if (count.get(x) == degree) {
+//         ans = Math.min(ans, right.get(x) - left.get(x) + 1);
+//     }
+// }
+
+// int x = 2;
+// ans = Math.min(ans , 8 - 1 + 1);          ans = 8;
+
+// int x = 3;
+// ans = Math.min(ans , 6 - 3 + 1);          ans = 4;
+
+
+// ans = 4;
